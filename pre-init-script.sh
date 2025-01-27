6218bf52fce3bd8c68dce1712d1817532e3317e3
Sean McGrath <sean@mcgrath.net.nz>
2025-01-27 23:31:36 +1300

    fix: update pre-init script instructions and modify Goss tests for CUPS service IP

--- pre-init-script.sh
+++ pre-init-script.sh
@@ -2,9 +2,9 @@
 
 echo "This is an example/dummy pre-init script. It does nothing by default.
 If you want to execute your own code & logic (e.g., installing a printer driver via a shell script or apt)
-before everything initializes and starts up 
+before everything initializes and starts up
 then:
     a) Replace this file via a volume mount (-v /path/to/host/script:/root/pre-init-script.sh)
-or  
-    b) Change the environment variable 'PRE_INIT_HOOK' to your custom (mounted) path or command 
+or
+    b) Change the environment variable 'PRE_INIT_HOOK' to your custom (mounted) path or command
        that should be executed inside the container."
